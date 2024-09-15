e7ab8b597321404463a76e0fd5e53f10de702922
aqsaa <aqsaatif@cmail.carleton.ca>
2024-09-15 18:24:24 -0400

    Added main method Updated main method

--- src/AddressBook.java
+++ src/AddressBook.java
@@ -13,5 +13,9 @@ public class AddressBook {
     }
     public static void main(String[] args){
         System.out.println("Address Book");
+        BuddyInfo buddy = new BuddyInfo();
+        AddressBook addressBook = new AdressBook();
+        addressBook.addBuddy(buddy);
+        addressBook.removeBuddy(buddy);
     }
 }
